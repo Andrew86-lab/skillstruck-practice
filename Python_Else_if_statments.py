43d4c11bb0db9484ff5cb061931f1a657c20161f
Andrew86-lab <andrewlgilljr@gmail.com>
2024-08-23 08:51:14 -0700

    exported from Skill Struck

---
+++ Python_Else_if_statments.py
@@ -0,0 +1,8 @@
+coin = 10
+
+if coin > 20:
+    print("You have more than enough to buy a puppy")
+elif coin == 20:
+    print("You have exactly enough to buy a puppy")
+else:
+    print("You do not have enough to buy a puppy")
