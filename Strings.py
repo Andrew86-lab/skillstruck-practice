7cdeef977f06b514350b78594ee7885acda5b6bf
Andrew86-lab <andrewlgilljr@gmail.com>
2024-08-23 08:51:13 -0700

    exported from Skill Struck

---
+++ Strings.py
@@ -0,0 +1,7 @@
+number1 = input("Give me an single digit number please: ")
+number2 = input("Give me an single digit number please: ")
+number3 = input("Give me an single digit number please: ")
+
+total = int(number1) + int(number2) + int(number3)
+
+print("The sum of those digits is {}.".format(total))
