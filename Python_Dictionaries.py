d37b2c746ed5e609cfe7e23f2a35e4604dc1edad
Andrew86-lab <andrewlgilljr@gmail.com>
2024-10-30 08:37:12 -0700

    exported from Skill Struck

---
+++ Python_Dictionaries.py
@@ -0,0 +1,10 @@
+cookies = {"Chocolate" : 1,
+           "White Chocolate" : 2,
+           "Dark Chocolate" : 3,
+           "Snickerdoodle" : 4,
+           "Sugar" : 5
+}
+
+fruits = ["Strawberry", "Apple", "Orange", "Mango", "Pineapple"]
+
+print(cookies), print(fruits)
