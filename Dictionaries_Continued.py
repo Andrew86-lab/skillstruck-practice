32a6a7c0301e82daf4bc38e506fd69c741161067
Andrew86-lab <andrewlgilljr@gmail.com>
2024-11-08 08:52:28 -0700

    exported from Skill Struck

---
+++ Dictionaries_Continued.py
@@ -0,0 +1,11 @@
+dictionary = {1: "bicycle", 2: "soccer balls", 3: "piano books"}
+
+item_4 = input("Enter something you have 4 of: ")
+item_5 = input("Enter something you have 5 of: ")
+item_6 = input("Enter something you have 6 of: ")
+
+dictionary[4] = item_4
+dictionary[5] = item_5
+dictionary[6] = item_6
+
+print(dictionary)
