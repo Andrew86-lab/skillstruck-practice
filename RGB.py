ad4a2e575bb1057ce7f28258a56f7f433929fbc9
Andrew86-lab <andrewlgilljr@gmail.com>
2025-01-23 08:13:20 -0700

    exported from Skill Struck

--- RGB.py
+++ RGB.py
@@ -1,10 +1,8 @@
-RGB = [int(n) for n in input("Give me numbers for RGB 0-255: ").split()]
+num = [int(n) for n in input("Give me the numbers for RGB 0-255: ").split()]
 
-result = [
+result = []
 
-]
-
-for element in RGB:
-    result.append(hex(element)[2:])
+for element in num:
+    result.append(hex(element)[2:].zfill(2))
 
 print("#" + "".join(result))
