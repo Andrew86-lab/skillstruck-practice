b6cc23a3a03b43247607dc848ac3bdadc89f5611
Andrew86-lab <andrewlgilljr@gmail.com>
2025-01-23 08:13:16 -0700

    exported from Skill Struck

--- Adding_To_and_Removing_From_Dictionaries.py
+++ Adding_To_and_Removing_From_Dictionaries.py
@@ -1,14 +1,2 @@
-group = {
-    "box1": 5,
-    "box2": 2,
-    "box3": 10,
-    "box4": 3,
-    "box5": int(input("Give the number: "))
-}
+answer = input("What do you want to say")
 
-total = 0
-
-for key in group:
-    total += 25*group.get(key)
-
-print(total)
