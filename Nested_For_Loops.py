7b0551056a2e0e7d5351e2f751fbfd979eff9ec1
Andrew86-lab <andrewlgilljr@gmail.com>
2025-01-29 10:32:21 -0700

    exported from Skill Struck

--- Nested_For_Loops.py
+++ Nested_For_Loops.py
@@ -1,3 +1,15 @@
-mylist = [1, 2, 3, 4, 5]
+# mylist = [1, 2, 3, 4, 5]
 
-rows = int(input("How many rows do you want: ").strip())
+# rows = int(input("How many rows do you want: ").strip())
+
+# list = [[(i * rows) for i in mylist] for i in range(1, rows + 1)]
+
+# print(list)
+
+# cols = ["windy", "breezy", "calm"]
+
+# rows = [item.strip() for item in input("Input a list of weather: ").split()]
+
+# list = [[(i + " " + j) for j in cols] for i in rows]
+
+# print(list)
